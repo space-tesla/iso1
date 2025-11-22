06b09da5ada99d6ca83b9052781c2aaeec093e18
wisdm-nikhil <nikhil.mhaske@gmail.com>
2025-11-22 21:55:11 +0530

    App Upload Done

---
+++ asyncapp/app.js
@@ -0,0 +1,18 @@
+const fs = require('fs').promises;
+
+async function readFiles() {
+  try {
+    const file1 = await fs.readFile('./data/file1.txt', 'utf-8');
+    const file2 = await fs.readFile('./data/file2.txt', 'utf-8');
+    const file3 = await fs.readFile('./data/file3.txt', 'utf-8');
+
+    console.log("File 1:", file1);
+    console.log("File 2:", file2);
+    console.log("File 3:", file3);
+    
+  } catch (err) {
+    console.log("Error Reading Files:", err);
+  }
+}
+
+readFiles();
